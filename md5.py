214c9ac549036c2b6adec6a998cf1ae9401f8f1f
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-09-12 18:56:45 +0330

    MD5 buffers_rotate

--- md5.py
+++ md5.py
@@ -4,6 +4,11 @@ class MD5:
     def __init__(self, string):
         print("MD5 constructor is called on {}".format(string))
         self.string = string
+        self.buffers_rotate =  [7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+			 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+			 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+			 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21]
 
     def get_string(self):
         return self.string
+
