e219f19e1f6e2d9ef813b57df617bcaa0cf2be12
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-09-12 19:24:48 +0330

    final test

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     string = "this is a test string"
     md5 = MD5(string)
 
-    print("MD5 hash of {} is {}".format(string, md5.md5()))
+    print("MD5 hash of {} is: {}".format(string, md5.md5()))
     print("The degree of agreement with the actual value: {}%".format(md5.evaluator()))
