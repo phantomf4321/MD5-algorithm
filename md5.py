be814db5a44023dc2ea62bab8875cb2934819973
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-09-12 18:54:39 +0330

    MD5 get_string

--- md5.py
+++ md5.py
@@ -4,3 +4,6 @@ class MD5:
     def __init__(self, string):
         print("MD5 constructor is called on {}".format(string))
         self.string = string
+
+    def get_string(self):
+        return self.string
