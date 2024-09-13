2e8097ffb5db0d30dc92b1208152f124c2b3431b
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-09-13 22:41:00 +0330

    add comment

--- main.py
+++ main.py
@@ -5,15 +5,5 @@ if __name__ == '__main__':
     string = "this is a test string"
     md5 = MD5(string)
 
-
-    def test_pad():
-        msg = bytearray("test", "ascii")
-        print(msg)
-        padded_msg = md5.pad(msg)
-        print(padded_msg)
-
-
-    test_pad()
-
     print("MD5 hash of {} is: {}".format(string, md5.md5()))
     print("The degree of agreement with the actual value: {}%".format(md5.evaluator()))
